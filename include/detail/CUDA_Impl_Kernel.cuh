a0a2904f87a2ed942aeda33dbdb9a54a117ce3df
Fangzhou Ai <fangzhouai@gmail.com>
2022-09-12 00:01:20 -0400

    Use cooperative_group to calculate stride

--- include/detail/CUDA_Impl_Kernel.cuh
+++ include/detail/CUDA_Impl_Kernel.cuh
@@ -1,7 +1,10 @@
 #pragma once
 #include <cuda_runtime.h>
+#include <cooperative_groups.h>
 #include <cuda.h>
 #include <complex>
+namespace cg = cooperative_groups;
+
 namespace CPRA
 {
 namespace Kernel
@@ -11,8 +14,8 @@ namespace Kernel
 template<typename T>
 __global__ void ker_SpaceConstraint(thrust::complex<T>* flat_src_data, T* flat_constr_data, uint64_t num, uint64_t batch_size)
 {
-    int thread_id =  blockIdx.x * blockDim.x + threadIdx.x;
-    for(uint64_t i = thread_id; i < num; i+= gridDim.x * blockDim.x)
+    
+    for(uint64_t i = cg::this_grid().thread_rank(); i < num; i+= cg::this_grid().size())
     {
         flat_src_data[i].imag(0);
         if(flat_constr_data[i % (num / batch_size)] == 0)
@@ -23,8 +26,7 @@ __global__ void ker_SpaceConstraint(thrust::complex<T>* flat_src_data, T* flat_c
 template<typename T>
 __global__ void ker_RealDataConstraint(thrust::complex<T>* flat_src_data, T* flat_constr_data, uint64_t num, uint64_t batch_size)
 {
-    int thread_id =  blockIdx.x * blockDim.x + threadIdx.x;
-    for(uint64_t i = thread_id; i < num; i+= gridDim.x * blockDim.x)
+    for(uint64_t i = cg::this_grid().thread_rank(); i < num; i+= cg::this_grid().size())
     {
         flat_src_data[i] = thrust::polar<T, T>(flat_constr_data[i % (num / batch_size)], thrust::arg(flat_src_data[i]));
     }
@@ -33,8 +35,7 @@ __global__ void ker_RealDataConstraint(thrust::complex<T>* flat_src_data, T* fla
 template<typename T>
 __global__ void ker_ComplexDataConstraint(thrust::complex<T>* flat_src_data, thrust::complex<T>* flat_constr_data, uint64_t num, uint64_t batch_size)
 {
-    int thread_id =  blockIdx.x * blockDim.x + threadIdx.x;
-    for(uint64_t i = thread_id; i < num; i+= gridDim.x * blockDim.x)
+    for(uint64_t i = cg::this_grid().thread_rank(); i < num; i+= cg::this_grid().size())
     {
         if(thrust::norm(flat_constr_data[i % (num / batch_size)]) != 0)
             flat_src_data[i] = flat_constr_data[i % (num / batch_size)];
@@ -44,8 +45,7 @@ __global__ void ker_ComplexDataConstraint(thrust::complex<T>* flat_src_data, thr
 template<typename T>
 __global__ void ker_MergeAddData(thrust::complex<T>* flat_src, thrust::complex<T>* flat_dst, T alpha, T beta, uint64_t num)
 {
-    int thread_id =  blockIdx.x * blockDim.x + threadIdx.x;
-    for(uint64_t i = thread_id; i < num; i+= gridDim.x * blockDim.x)
+    for(uint64_t i = cg::this_grid().thread_rank(); i < num; i+= cg::this_grid().size())
     {
         flat_dst[i] = flat_src[i] * alpha + flat_dst[i] * beta;
     }
@@ -54,8 +54,7 @@ __global__ void ker_MergeAddData(thrust::complex<T>* flat_src, thrust::complex<T
 template<typename T>
 __global__ void ker_Normalization(thrust::complex<T>* flat_src, T norm, uint64_t num)
 {
-    int thread_id =  blockIdx.x * blockDim.x + threadIdx.x;
-    for(uint64_t i = thread_id; i < num; i+= gridDim.x * blockDim.x)
+    for(uint64_t i = cg::this_grid().thread_rank(); i < num; i+= cg::this_grid().size())
     {
         flat_src[i] /= norm;
     }
